d2d79949a924f6e3530274c08a5a79c9ab6c9ba4
jonijani <junaidzaib.web@gmail.com>
2023-06-24 23:51:06 +0100

    created function get_res() to see if url actually return 200 ok code_sttaus or not

--- bs4_test.py
+++ bs4_test.py
@@ -3,7 +3,14 @@ import requests
 BASE_URL='http://search.wanteddistributors.com'
 
 
+# print(res.status_code)
+# print(res.text)
 
+def get_resp(url):
+    res = requests.get(url)
+    if res.status_code == 200:
+        return res
+    return None
 
 
 
@@ -14,7 +21,8 @@ if __name__ == '__main__':
    user_input = input('enter keyword to search :')
    url = f"{BASE_URL}/search/?q={user_input}"
    print(url)
+   print(get_resp(url).text)
 
 
 
-   
+
