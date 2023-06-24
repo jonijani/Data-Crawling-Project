17f6385c7e73e43decb8e0b8e4f00e5ad68baab4
jonijani <junaidzaib.web@gmail.com>
2023-06-24 23:44:32 +0100

    created user input to search for keywords and added print functio to see if it works or not

--- bs4_test.py
+++ bs4_test.py
@@ -1,3 +1,20 @@
 import requests
 
 BASE_URL='http://search.wanteddistributors.com'
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+   # http://search.wanteddistributors.com/search/?q=hardware
+   user_input = input('enter keyword to search :')
+   url = f"{BASE_URL}/search/?q={user_input}"
+   print(url)
+
+
+
+   
